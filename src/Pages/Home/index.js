89deb007e94bda1cd794021ac31de8e74f7978b1
KireMitrov <kire_val@yahoo.com>
2022-08-15 13:43:22 +0200

    Initial commit

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+function Home(){
+    return (
+        <div className="home-container">
+            <div className="left">
+                <div className="home-text">
+                    <p className="home-text-main">Make <br/> remote work</p>
+                    <p className='home-text-info'>Get your team in sync, no matter your location. Streamline processes, create team rituals, and <br/>watch productivity soar.</p>
+                </div>
+                    <button className="btn-learn">Learn more</button>
+                <div className="home-logos">
+                    <img src="images/client-databiz.svg" alt="databiz-logo"></img>
+                    <img src="images/client-audiophile.svg" alt="audiophile-logo"></img>
+                    <img src="images/client-meet.svg" alt="meet-logo"></img>
+                    <img src="images/client-maker.svg" alt="maker-logo"></img>
+                </div>
+            </div>
+            <div className="image-right">
+                <img src="images/image-hero-desktop.png"></img>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Header(){
+    return (
+        <div className='header'>
+            <div className='menu'>
+            <img src="images/logo.svg" alt='snap-logo'></img>
+                <p>Features <img src='images/icon-arrow-down.svg' alt='arrow-down'/></p>
+                <p>Company <img src='images/icon-arrow-down.svg' alt='arrow-down'/></p>
+                <p>Careers</p>
+                <p>About</p>
+            </div>
+            <div className='authorisation'>
+                <p>Login</p>
+                <p>Register</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header
